daaa631688e6a731eccbba14547bedd50f2dbd4a
akram_rana <akram686202@gmail.com>
2017-04-22 16:38:48 +0600

    Update ahr.php

--- ahr.php
+++ ahr.php
@@ -3,11 +3,10 @@
 /**
  * @author Akram Hossain <akram_cse@yahoo.com>
  * @copyright (c) 2014, Akram Hossain
- * @uses Generate Yii(1) Model From Oracle Database.If the database size is huge as like me 
+ * @uses Generate Yii(1) Model From Oracle Database.If the database size is huge as like 
  * 300+ then Yii(1) unable to generate the model. for this reason i made this script 
  * to helps developer to generate Yii(1) model and saves there time.
  * @version PHP 5.6.3, ORACLE 11.2g
- * @license Feel free to modify and use of this script   
  */
 
 /**
